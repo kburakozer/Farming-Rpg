4a01f7067a4b908052ea70be5c9f548590920ede
kburakozer <kburakozer@yahoo.com>
2025-01-11 21:13:10 +0300

    Animation controller and Player movement part 1

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -4,5 +4,46 @@ using UnityEngine;
 
 public class Player : SingletonMonoBehaviour<Player>
 {
+    // Movement Parameters
+    private float xInput;
+
+    private float yInput;
+    private bool isCarrying = false;
+    private bool isIdle;
+    private bool isLiftingToolDown;
+    private bool isLiftingToolLeft;
+    private bool isLiftingToolRight;
+    private bool isLiftingToolUp;
+    private bool isRunning;
+    private bool isUsingToolDown;
+    private bool isUsingToolLeft;
+    private bool isUsingToolRight;
+    private bool isUsingToolUp;
+    private bool isSwingingToolDown;
+    private bool isSwingingToolLeft;
+    private bool isSwingingToolRight;
+    private bool isSwingingToolUp;
+    private bool isWalking;
+    private bool isPickingUp;
+    private bool isPickingDown;
+    private bool isPickingLeft;
+    private bool isPickingRight;
+    private ToolEffect toolEffect = ToolEffect.none;
     
+    private Rigidbody2D rigidBody2D;
+    
+    private Direction playerDirection;
+    
+    private float movementSpeed;
+    
+    private bool _playerInputIsDisabled = false;
+    public bool PlayerInputIsDisabled { get => _playerInputIsDisabled; set => _playerInputIsDisabled = value; }
+
+    protected override void Awake()
+    {
+        // It calls the base class method
+        base.Awake();
+
+        rigidBody2D = GetComponent<Rigidbody2D>();
+    }
 }
