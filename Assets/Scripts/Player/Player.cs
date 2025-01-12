07d9d295284c95628575ffc1e0c022d6447d9b1a
kburakozer <kburakozer@yahoo.com>
2025-01-12 13:58:41 +0300

    Player movement completed

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -6,7 +6,6 @@ public class Player : SingletonMonoBehaviour<Player>
 {
     // Movement Parameters
     private float xInput;
-
     private float yInput;
     private bool isCarrying = false;
     private bool isIdle;
@@ -45,5 +44,122 @@ public class Player : SingletonMonoBehaviour<Player>
         base.Awake();
 
         rigidBody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        #region Player Input
+
+        ResetAnimationTriggers();
+        PlayerMovementInput();
+        PlayerWalkInput();
+        
+        // Send event to any listeners for player movement input
+        EventHandler.CallMovementEvent(xInput, yInput, isWalking, isRunning, isIdle, isCarrying, toolEffect,
+            isUsingToolRight, isUsingToolLeft, isUsingToolUp, isUsingToolDown,
+            isLiftingToolRight, isLiftingToolLeft, isLiftingToolUp, isLiftingToolDown,
+            isPickingRight, isPickingLeft, isPickingUp, isPickingDown,
+            isSwingingToolRight, isSwingingToolLeft, isSwingingToolUp, isSwingingToolDown,
+            false, false, false, false);
+
+        #endregion Player Input
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        Vector2 move = new Vector2(xInput * movementSpeed * Time.deltaTime, yInput * movementSpeed * Time.deltaTime);
+
+        rigidBody2D.MovePosition(rigidBody2D.position + move);
+    }
+    
+    private void ResetAnimationTriggers()
+    {
+        isPickingRight = false;
+        isPickingLeft = false;
+        isPickingUp = false;
+        isPickingDown = false;
+        isUsingToolRight = false;
+        isUsingToolLeft = false;
+        isUsingToolUp = false;
+        isUsingToolDown = false;
+        isLiftingToolRight = false;
+        isLiftingToolLeft = false;
+        isLiftingToolUp = false;
+        isLiftingToolDown = false;
+        isSwingingToolRight = false;
+        isSwingingToolLeft = false;
+        isSwingingToolUp = false;
+        isSwingingToolDown = false;
+        toolEffect = ToolEffect.none;
+    }
+    
+    private void PlayerMovementInput()
+    {
+        // Values will be either 0, 1 or -1
+        yInput = Input.GetAxisRaw("Vertical");
+        xInput = Input.GetAxisRaw("Horizontal");
+
+        // Measn pressing both the  vertical and horizontal input
+        if (yInput != 0 && xInput != 0)
+        {
+            // 0.71 is the distance on diagonal axis
+            xInput = xInput * 0.71f;
+            yInput = yInput * 0.71f;
+        }
+
+        if (xInput != 0 || yInput != 0)
+        {
+            isRunning = true;
+            isWalking = false;
+            isIdle = false;
+            movementSpeed = Settings.runningSpeed;
+
+            // Capture player direction for save game
+            if (xInput < 0)
+            {
+                playerDirection = Direction.left;
+            }
+            else if (xInput > 0)
+            {
+                playerDirection = Direction.right;
+            }
+            else if (yInput < 0)
+            {
+                playerDirection = Direction.down;
+            }
+            else
+            {
+                playerDirection = Direction.up;
+            }
+        }
+        else if (xInput == 0 && yInput == 0)
+        {
+            isRunning = false;
+            isWalking = false;
+            isIdle = true;
+        }
+    }
+    
+    private void PlayerWalkInput()
+    {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift))
+        {
+            isRunning = false;
+            isWalking = true;
+            isIdle = false;
+            movementSpeed = Settings.walkingSpeed;
+        }
+        else
+        {
+            isRunning = true;
+            isWalking = false;
+            isIdle = false;
+            movementSpeed = Settings.runningSpeed;
+        }
     }
 }
